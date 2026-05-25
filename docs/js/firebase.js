e55ae0d405ab3b51a2910b80f4b40bbb6bb9748a
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-05-25 14:44:49 -0400

    Create firebase.js

---
+++ docs/js/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD39ihyJvTmy3kERj_Ct4SPZkYFIpUgMjM",
+  authDomain: "pool-hockey-5d1d0.firebaseapp.com",
+  projectId: "pool-hockey-5d1d0",
+  storageBucket: "pool-hockey-5d1d0.firebasestorage.app",
+  messagingSenderId: "1030892892347",
+  appId: "1:1030892892347:web:c88a5cbcb25fe63ec1799e",
+  measurementId: "G-QEFCJEDYNG"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+window.firebase = { auth, db, signInWithPopup, GoogleAuthProvider };
