55aa4074c040227ad174865d21ca8466b1b9bcb6
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-05-25 14:49:21 -0400

    Update firebase.js

--- docs/js/firebase.js
+++ docs/js/firebase.js
@@ -1,5 +1,6 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -9,11 +10,10 @@ const firebaseConfig = {
   storageBucket: "pool-hockey-5d1d0.firebasestorage.app",
   messagingSenderId: "1030892892347",
   appId: "1:1030892892347:web:c88a5cbcb25fe63ec1799e",
-  measurementId: "G-QEFCJEDYNG"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-window.firebase = { auth, db, signInWithPopup, GoogleAuthProvider };
+export { auth, db, GoogleAuthProvider };
