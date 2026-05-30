267fa89cde012da844e00498235f632778c08230
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-05-30 17:48:57 -0400

    Update render.js

--- docs/js/ui/render.js
+++ docs/js/ui/render.js
@@ -2,7 +2,7 @@ import { appState } from "../app/state.js";
 import { getAllPredictions } from "../services/firestoreService.js";
 import { SCORING } from "../constants.js";
 import { computeLeaderboard } from "../logic/scoring.js";
-import { isResultAvailable, getParent } from "../utils/helpers.js"; // temporaire
+import { isResultAvailable, getParentMatch} from "../utils/helpers.js"; // temporaire
 
 
 export async function loadPredictionsDetails() {
