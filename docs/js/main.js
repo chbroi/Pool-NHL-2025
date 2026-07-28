f4f0ed00548a7376544838099316cfc6086b6e77
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-07-28 17:50:40 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -264,7 +264,6 @@ if (rules) rules.style.display = "none";
 
   if (tabName === "rules") {
     document.getElementById("rulesContainer").style.display = "block";
-    document.getElementById("acceptRulesButton").style.display = "none";
     document.getElementById("engagementContainer").style.display = "none";
 
   }
