99e4fa19b98b8510c8fe45cf9f4d2bb046f51c1b
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-08-02 12:10:08 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -883,3 +883,15 @@ async function(id) {
   renderAdmin();
 
 };
+
+window.testNhlApi = async function() {
+
+  const response = await fetch(
+    "https://api-web.nhle.com/v1/skater-stats-leaders/current"
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+
+};
