fc170082836893b1af9007837eb602ee8d0d8ef9
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-08-01 20:03:19 -0400

    Update functions.js

--- docs/js/functions.js
+++ docs/js/functions.js
@@ -411,3 +411,22 @@ export function refreshHelperMessage() {
       "🔒 Prédictions terminées. Revenez plus tard pour la prochaine ronde.";
   }
 }
+
+export function formatDateTimeLocal(timestamp) {
+
+  const d = new Date(timestamp);
+
+  const pad = n => String(n).padStart(2,"0");
+
+  return `${d.getFullYear()}-${
+    pad(d.getMonth()+1)
+  }-${
+    pad(d.getDate())
+  }T${
+    pad(d.getHours())
+  }:${
+    pad(d.getMinutes())
+  }`;
+
+}
+``
