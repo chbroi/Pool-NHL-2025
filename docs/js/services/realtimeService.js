21d7b4ecefc17a46367a446fc44605137bf21383
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-08 16:07:27 -0400

    Create realtimeService.js

---
+++ docs/js/services/realtimeService.js
@@ -0,0 +1,77 @@
+import { db } from "../firebase.js";
+
+import {
+  doc,
+  collection,
+  onSnapshot
+}
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import { appState } from "../app/state.js";
+import * as funcs from "../functions.js";
+
+import {
+  renderHome,
+  renderAdmin,
+  renderFullLeaderboard,
+  loadPredictionsDetails
+}
+from "../ui/render.js";
+
+export function setupRealtimeListeners() {
+
+  // Config
+
+  onSnapshot(
+    doc(db, "config", "ui"),
+    (snap) => {
+
+      const config = snap.data();
+
+      if (!config) return;
+
+      appState.submission =
+        Number(config.currentSubmission);
+
+      appState.submissionOpen =
+        config.submissionOpen;
+
+      appState.round1Deadline =
+        config.round1Deadline;
+
+      appState.round2Deadline =
+        config.round2Deadline;
+
+      appState.round3Deadline =
+        config.round3Deadline;
+
+      appState.round4Deadline =
+        config.round4Deadline;
+
+      funcs.refreshHelperMessage();
+
+    }
+  );
+
+  // Results
+
+  onSnapshot(
+    doc(db, "results", "Current"),
+    (snap) => {
+
+      const data = snap.data();
+
+      if (!data) return;
+
+      appState.results = data;
+
+      renderHome();
+
+      renderFullLeaderboard();
+
+      loadPredictionsDetails();
+
+    }
+  );
+
+}
