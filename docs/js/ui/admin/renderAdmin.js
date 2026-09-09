dbc4d6e48024792c74225ca258f050cb2762da83
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-09 12:23:25 -0400

    Update renderAdmin.js

--- docs/js/ui/admin/renderAdmin.js
+++ docs/js/ui/admin/renderAdmin.js
@@ -1,3 +1,8 @@
+import { appState } from "../../app/state.js";
+import { renderAdminSubmissionCard } from "./renderAdminSubmission.js";
+import { renderAdminPaymentsCard, loadAdminPayments } from "./renderAdminPayments.js";
+import { renderAdminFeedbackCard, loadAdminFeedback } from "./renderAdminFeedback.js";
+import { renderAdminHistoryCard, loadAdminHistory } from "./renderAdminHistory.js";
 
 export async function renderAdmin() {
 
