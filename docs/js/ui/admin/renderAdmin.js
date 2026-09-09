81fcd9f6f71020ec51b2b9f1ffb8afefeb58476b
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-09 12:03:32 -0400

    Update renderAdmin.js

--- docs/js/ui/admin/renderAdmin.js
+++ docs/js/ui/admin/renderAdmin.js
@@ -1,1 +1,18 @@
 
+export async function renderAdmin() {
+
+  const container =
+    document.getElementById("adminTab");
+
+  container.innerHTML = `
+    ${renderAdminStatus()}
+    ${renderAdminSubmissions()}
+    ${renderAdminPaymentsCard()}
+    ${renderAdminFeedbackCard()}
+    ${renderAdminHistoryCard()}
+  `;
+
+  await loadAdminPayments();
+  await loadAdminFeedback();
+  await loadAdminHistory();
+}
