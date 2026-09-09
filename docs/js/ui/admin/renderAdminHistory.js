042a655ad8268a7280d77ecfbe457c13e17b6bde
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-09 12:05:25 -0400

    Update renderAdminHistory.js

--- docs/js/ui/admin/renderAdminHistory.js
+++ docs/js/ui/admin/renderAdminHistory.js
@@ -1,1 +1,54 @@
 
+export function renderAdminHistoryCard() {
+
+  return `
+    <div class="card">
+
+      <h3>
+        📜 Historique admin
+      </h3>
+
+      <div id="adminHistoryContainer">
+
+      </div>
+
+      <br>
+
+      <button
+        class="actionBtn"
+        onclick="clearAdminHistory()"
+      >
+        Supprimer l'historique
+      </button>
+
+    </div>
+  `;
+}
+
+export async function loadAdminHistory() {
+
+  const logs =
+    await getAdminLogs();
+
+  const container =
+    document.getElementById(
+      "adminHistoryContainer"
+    );
+
+  container.innerHTML = "";
+
+  logs
+    .sort(
+      (a,b) =>
+      b.timestamp-a.timestamp
+    )
+    .forEach(log => {
+
+      container.innerHTML += `
+        <div>
+          ${log.action}
+        </div>
+      `;
+
+    });
+}
