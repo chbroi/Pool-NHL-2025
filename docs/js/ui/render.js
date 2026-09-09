87d6732473f506752898fd235ab4f92fb804090c
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-09 14:52:05 -0400

    Update render.js

--- docs/js/ui/render.js
+++ docs/js/ui/render.js
@@ -2,7 +2,7 @@ export { renderHome } from "./home/renderHome.js";
 export { renderAdmin } from "./admin/renderAdmin.js";
 export { renderProfile } from "./profile/renderProfile.js";
 export { renderFullLeaderboard } from "./leaderboard/renderLeaderboard.js";
-export { loadPredictionsDetails }from "./results/renderResults.js";
+export { loadPredictionsDetails,generateRound}from "./results/renderResults.js";
 export { renderStats } from "./stats/renderPoolStats.js";
 export { renderNhlStats, renderNhlStatsTable, attachNhlStatsListeners} from "./stats/renderNhlStats.js";
 export { reloadFeedbackSection } from "./admin/renderAdminFeedback.js";
