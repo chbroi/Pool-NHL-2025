485db419d9746c7ec25286916dbe3af927386741
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-09 12:56:07 -0400

    Update renderAdminPayments.js

--- docs/js/ui/admin/renderAdminPayments.js
+++ docs/js/ui/admin/renderAdminPayments.js
@@ -1,1 +1,65 @@
+export function renderAdminPaymentsCard() {
 
+  return `
+
+    <div class="card">
+
+      <h3>
+        💰 Gestion des paiements
+      </h3>
+
+      <div id="paymentsContainer">
+
+      </div>
+
+    </div>
+
+  `;
+}
+
+export async function loadAdminPayments() {
+
+  const participants =
+    await getAllParticipants();
+
+  const container =
+    document.getElementById(
+      "paymentsContainer"
+    );
+
+  container.innerHTML = "";
+
+  participants.forEach(p => {
+  
+  paymentsContainer.innerHTML += `
+  
+    <div>
+  
+      <label>
+  
+        <input
+          type="checkbox"
+          ${
+            p.paid
+              ? "checked"
+              : ""
+          }
+          onchange="
+            togglePayment(
+              '${p.id}',
+              this.checked
+            )
+          "
+        >
+  
+        ${p.name || p.displayName}
+  
+      </label>
+  
+    </div>
+  
+  `;
+  
+  });;
+
+}
