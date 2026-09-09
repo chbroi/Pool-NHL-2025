77065c7ac6546975758cdea579dfa28a4ca8f429
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-09 13:07:33 -0400

    Update renderAdminSubmission.js

--- docs/js/ui/admin/renderAdminSubmission.js
+++ docs/js/ui/admin/renderAdminSubmission.js
@@ -154,3 +154,22 @@ export function renderAdminSubmissionCard() {
         </div>
   `;
 }
+
+
+export function formatDateTimeLocal(timestamp) {
+
+  const d = new Date(timestamp);
+
+  const pad = n => String(n).padStart(2,"0");
+
+  return `${d.getFullYear()}-${
+    pad(d.getMonth()+1)
+  }-${
+    pad(d.getDate())
+  }T${
+    pad(d.getHours())
+  }:${
+    pad(d.getMinutes())
+  }`;
+
+}
