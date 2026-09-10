0fc928062cc9f4b24b25b64ce8254dc92e46e5d7
Charles Brosseau <57077123+chbroi@users.noreply.github.com>
2026-09-10 17:31:24 -0400

    Create adminActions.js

---
+++ docs/js/admin/adminActions.js
@@ -0,0 +1,267 @@
+window.toggleSubmissionOpen = async function(status) {
+  
+  await updateDoc(
+    doc(db, "config", "ui"),
+    {
+      submissionOpen: status
+    }
+  );
+  await addDoc(
+  collection(db, "adminLogs"),
+  {
+    action: status
+      ? "Ouverture des soumissions"
+      : "Fermeture des soumissions",
+
+    admin:
+      appState.user.displayName,
+
+    timestamp:
+      Date.now()
+  }
+);
+
+  appState.submissionOpen = status;
+  renderAdmin();
+if (document.getElementById("submitTab").style.display === "block") {
+showTab("submit");
+}
+  const helper =
+  document.getElementById(
+    "helperMessage"
+  );
+
+if (helper) {
+
+  if (status) {
+    const currentDeadline = appState[
+    `round${appState.submission}Deadline`];
+
+    helper.innerHTML =
+      `⏳ Vous avez jusqu'au ${
+        new Date(
+          currentDeadline
+        ).toLocaleString()
+      } pour soumettre vos prédictions.`;
+
+  } else {
+
+    helper.innerHTML =
+      "🔒 Prédictions terminées. Revenez plus tard.";
+
+  }
+}
+  
+  alert(
+    status
+    ? "Soumissions ouvertes"
+    : "Soumissions fermées"
+  );
+funcs.refreshHelperMessage();
+};
+
+window.updateSubmissionRound =
+async function() {
+
+  const round = Number(
+    document.getElementById(
+      "adminSubmission"
+    ).value
+  );
+
+  await updateDoc(
+    doc(db, "config", "ui"),
+    {
+      currentSubmission: round
+    }
+  );
+  await addDoc(
+  collection(db, "adminLogs"),
+  {
+    action:
+      `Soumission active -> ${round}`,
+
+    admin:
+      appState.user.displayName,
+
+    timestamp:
+      Date.now()
+  }
+);
+
+  appState.submission = round;
+  renderAdmin();
+  funcs.refreshHelperMessage();
+
+  alert(
+    `Soumission ${round} activée`
+  );
+  
+};
+
+window.clearAdminHistory =
+async function() {
+
+  if (
+    !confirm(
+      "Supprimer tout l'historique ?"
+    )
+  ) {
+    return;
+  }
+
+  const snapshot =
+    await getDocs(
+      collection(
+        db,
+        "adminLogs"
+      )
+    );
+
+  await Promise.all(
+
+    snapshot.docs.map(
+      d =>
+        deleteDoc(d.ref)
+    )
+
+  );
+
+  renderAdmin();
+
+};
+
+
+window.updateDeadline =
+async function() {
+
+  await addDoc(
+  collection(db, "adminLogs"),
+  {
+    action:
+      "Modification des dates limites",
+
+    admin:
+      appState.user.displayName,
+
+    timestamp:
+      Date.now()
+  }
+);
+
+
+  await updateDoc(
+    doc(db, "config", "ui"),
+    {
+
+      round1Deadline:
+        new Date(
+          document.getElementById(
+            "round1Deadline"
+          ).value
+        ).getTime(),
+
+      round2Deadline:
+        new Date(
+          document.getElementById(
+            "round2Deadline"
+          ).value
+        ).getTime(),
+
+      round3Deadline:
+        new Date(
+          document.getElementById(
+            "round3Deadline"
+          ).value
+        ).getTime(),
+
+      round4Deadline:
+        new Date(
+          document.getElementById(
+            "round4Deadline"
+          ).value
+        ).getTime()
+
+    }
+  );
+
+  alert(
+    "Dates mises à jour"
+  );
+funcs.refreshHelperMessage();
+};
+
+
+window.deletePredictionAdmin =
+async function() {
+
+  const id =
+    document.getElementById(
+      "deletePredictionSelect"
+    ).value;
+
+  if (
+    !confirm(
+      "Supprimer cette soumission ?"
+    )
+  ) {
+    return;
+  }
+
+  await deleteDoc(
+    doc(
+      db,
+      "predictions",
+      id
+    )
+  );
+
+  renderAdmin();
+
+  alert(
+    "Soumission supprimée."
+  );
+
+};
+
+window.togglePayment =
+async function(
+  uid,
+  paid
+) {
+
+  await updateDoc(
+    doc(
+      db,
+      "participants",
+      uid
+    ),
+    {
+      paid
+    }
+  );
+
+};
+
+window.deleteFeedback =
+async function(id) {
+
+  if (
+    !confirm(
+      "Supprimer ce commentaire ?"
+    )
+  ) {
+    return;
+  }
+
+  await deleteDoc(
+    doc(
+      db,
+      "feedback",
+      id
+    )
+  );
+
+  renderAdmin();
+
+};
